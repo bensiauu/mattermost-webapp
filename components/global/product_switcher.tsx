3afac928c57b119669b7ad47ffdde6db436e0d78
Michel Engelen <jsnerdic@gmail.com>
2021-08-23 10:07:43 +0200

    re-added onClick menu closing functionality to menu items

--- components/global/product_switcher.tsx
+++ components/global/product_switcher.tsx
@@ -164,6 +164,7 @@ const ProductSwitcher = (): JSX.Element => {
                     <ProductSwitcherMenu
                         id='ProductSwitcherMenu'
                         isMessaging={currentProductID === null}
+                        onClick={handleClick}
                     />
                 </Menu>
             </MenuWrapper>
